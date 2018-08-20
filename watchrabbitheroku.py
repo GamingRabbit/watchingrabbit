f89bcca3fb7e27b93de807cb76f00d019cc15916
GamingRabbit <42521007+GamingRabbit@users.noreply.github.com>
2018-08-20 18:41:37 +0200

    Update watchrabbitheroku.py

--- watchrabbitheroku.py
+++ watchrabbitheroku.py
@@ -17,19 +17,19 @@ async def on_message(message):
     channel = discord.Object(id=os.getenv("Chan"))
     if message.author.bot:
         pass
-    elif(message.author.id == os.getenv("Usero"):
+    elif(message.author.id == os.getenv("Usero")):
         pass
-    elif(message.author.id == os.getenv("Usert"):
+    elif(message.author.id == os.getenv("Usert")):
         pass
-    elif(message.author.id == os.getenv("Userth"):
+    elif(message.author.id == os.getenv("Userth")):
         pass
-    elif(message.author.id == os.getenv("Userf"):
+    elif(message.author.id == os.getenv("Userf")):
         pass
-    elif(message.author.id == os.getenv("Userfi"):
+    elif(message.author.id == os.getenv("Userfi")):
         pass
-    elif(message.author.id == os.getenv("Usersi"):
+    elif(message.author.id == os.getenv("Usersi")):
         pass
-    elif(message.author.id == os.getenv("Userse"):
+    elif(message.author.id == os.getenv("Userse")):
         pass
     else:
         await client.send_message(channel, '{0} wrote {1}'.format(message.author, message.content))
